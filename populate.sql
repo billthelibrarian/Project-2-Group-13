588aad0efc271b5179b4f9f3f2c5240f0022cf1f
John <107654605+JohnTorgerson@users.noreply.github.com>
2022-09-14 19:39:23 -0500

    added populate code, organized.csv

---
+++ populate.sql
@@ -0,0 +1,18 @@
+drop table courses;
+
+CREATE TABLE courses (
+	university VARCHAR,
+	campus VARCHAR,
+	campus_id VARCHAR,
+	term VARCHAR,
+	term_id VARCHAR,
+	department VARCHAR,
+	department_id VARCHAR,
+	course VARCHAR,
+	course_id VARCHAR,
+	section VARCHAR,
+	section_id VARCHAR
+);
+
+select * from courses;
+
